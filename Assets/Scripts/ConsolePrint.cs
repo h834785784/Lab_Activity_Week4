022754443735f4a2801d82c870451331a641d993
Snart <snart.jian.hong@gmail.com>
2021-08-29 22:20:58 +0800

    80percentcomplete

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    public GameObject objname;
+    private int i;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 3;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i +=1;
+        Debug.Log(objname.name+ ":" +i);
+
+    }
+}
